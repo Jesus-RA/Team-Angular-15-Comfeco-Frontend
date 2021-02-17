490591fd2896b557cccfaf3d64bbf4f05c32bf03
Jesus-RA <jesus.ra98@hotmail.com>
2021-02-17 17:10:45 -0600

    Created privacy and data protection policy page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 
 const routes: Routes = [
   { path:"login", component: LoginComponent },
+  { path:"politica-de-privacidad-y-proteccion-de-datos", component: PrivacyPolicyComponent },
   { path:"**", pathMatch:"full", redirectTo:"home" }
 ];
 

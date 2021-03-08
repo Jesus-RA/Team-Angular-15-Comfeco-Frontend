8781cdb1f43116581644265383609b83b9d71aed
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-08 16:39:38 -0600

    Added new variables environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  url: "http://localhost:5050/api/v1"
+  url: "http://localhost:5050/api/v1",
+  GOOGLE_CLIENT_ID: "495092721571-pl56bohv6e5853s923d5k69d6heuqtbp.apps.googleusercontent.com",
+  FACEBOOK_CLIENT_ID: "1144377869337240"
 };
 
 /*

b1b1955981b930b12dde47c93fdb90b6b646e850
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-09 20:40:41 -0600

    new added component in material

--- src/app/material/material.ts
+++ src/app/material/material.ts
@@ -3,10 +3,12 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatIconModule } from "@angular/material/icon";
+import { MatTabsModule } from "@angular/material/tabs";
 
 export const AngularMaterial = [
     MatButtonModule,
     MatDialogModule,
     MatMenuModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule
 ];

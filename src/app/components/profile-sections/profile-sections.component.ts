0629b4f82bd61c38ceed31166b226051cd738daf
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-09 21:03:57 -0600

    New component added

---
+++ src/app/components/profile-sections/profile-sections.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-sections',
+  templateUrl: './profile-sections.component.html',
+  styleUrls: ['./profile-sections.component.css']
+})
+export class ProfileSectionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

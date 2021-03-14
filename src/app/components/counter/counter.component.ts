3c19c60aabe6c45356994f6380362d90abca10a5
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-14 17:00:31 -0600

    Added functionality to counter.

--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,3 +1,4 @@
+// Imports modules.
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
-
-  constructor() { }
+  days: string = "";
+  hours: string = "";
+  minutes: string = "";
+  seconds: string = "";
 
   ngOnInit(): void {
+    this.countdown();
   }
 
+  private countdown(): void {
+    setInterval(() => {
+      const currentTime: number = new Date().getTime();
+      const eventTime: number = new Date(2022, 0, 1).getTime();
+      const timeLeft: number = eventTime - currentTime;
+      this.conversion(timeLeft);
+    }, 1000);
+  }
+
+  private conversion(timeLeft: number): void {
+    let seconds: number = Math.floor(timeLeft / 1000);
+    let minutes: number = Math.floor(seconds / 60);
+    let hours: number = Math.floor(minutes / 60);
+    let days: number = Math.floor(hours / 24);
+
+    hours %= 60;
+    minutes %= 60;
+    seconds %= 60;
+    
+    this.days = this.parseData(days)
+    this.hours = this.parseData(hours);
+    this.minutes = this.parseData(minutes);
+    this.seconds = this.parseData(seconds);
+  }
+
+  private parseData(number: number): string {
+    return number < 10  ? `0${ number }` : `${ number }`;
+  }
 }

6de28ab230a8ef5c1eb64d11745db631549d03e0
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-13 21:32:53 -0600

    The welcome banner is modified.

--- src/app/components/welcome-banner/welcome-banner.component.ts
+++ src/app/components/welcome-banner/welcome-banner.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+// Imports modules.
+import { Component } from '@angular/core';
+
+// Imports services.
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-welcome-banner',
   templateUrl: './welcome-banner.component.html',
   styleUrls: ['./welcome-banner.component.css']
 })
-export class WelcomeBannerComponent implements OnInit {
+export class WelcomeBannerComponent {
+  user: any = {};
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authService: AuthService) {
+    this.authService.currentUser.subscribe(user => this.user = user);
   }
-
 }

1e551e1d607efc18614a4e6f0a4016642b0a8c73
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-14 13:47:18 -0600

    Carousel is styled for content creators.

--- src/app/components/carousel-creators-content/carousel-creators-content.component.ts
+++ src/app/components/carousel-creators-content/carousel-creators-content.component.ts
@@ -1,3 +1,4 @@
+// Imports modules.
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +6,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './carousel-creators-content.component.html',
   styleUrls: ['./carousel-creators-content.component.css']
 })
-export class CarouselCreatorsContentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class CarouselCreatorsContentComponent {
+  creatorsContent = [
+    {
+      name: "Bezael Perez",
+      avatar: "https://pbs.twimg.com/profile_images/1275143505288089600/3YCxeMFu.jpg"
+    },
+    {
+      name: "Nicolas Schurmann",
+      avatar: "https://s.gravatar.com/avatar/a9484e5077527b9d4d8bdc7c272fc16d?size=496&default=retro"
+    },
+    {
+      name: "Diego Montoya",
+      avatar: "https://www.eafit.edu.co/thetransmediaearthconference/PublishingImages/Paginas/default/diego-montoya.png"
+    },
+    {
+      name: "Sacha Lifszync",
+      avatar: "https://avatars.githubusercontent.com/u/1213542?s=400&v=4"
+    }
+  ];
 }

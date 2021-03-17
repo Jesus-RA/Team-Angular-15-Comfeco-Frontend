30adf1d43b3248e01dca96af38685c55e1173a56
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-17 13:58:25 -0600

    Badge earning has been configured.

--- src/app/components/profile-sections/profile-sections.component.ts
+++ src/app/components/profile-sections/profile-sections.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from 'src/app/services/auth/auth.service';
 import { BadgeService } from 'src/app/services/badge/badge.service';
 import { Badge } from 'src/app/services/badge/interfaces/badge.interfaces';
 import { DetailsUser } from '../details-user-section/interfaces/details-user.interfaces';
+import { StickerSection } from '../general-sticker-section/interfaces/stickerSection.interfaces';
 
 @Component({
   selector: 'app-profile-sections',
@@ -14,7 +15,11 @@ import { DetailsUser } from '../details-user-section/interfaces/details-user.int
   encapsulation: ViewEncapsulation.None
 })
 export class ProfileSectionsComponent implements OnInit {
-  myBadges: Badge[] = [];
+  myInsigniaSection: StickerSection = {
+    header: { icon: "military_tech", title: "Mis insignias" },
+    stickers: []
+  };
+
   badges: Badge[] = [];
   detailsUser: DetailsUser = {
     header: { icon: "info", title: "Detalles" },
@@ -34,8 +39,14 @@ export class ProfileSectionsComponent implements OnInit {
         { icon: "calendar_today", text: user.birthday?.toString() }
       ];
 
-      this.badgeService.getByUserId(user._id).subscribe(res => {
-        this.myBadges = res.badges;
+      this.badgeService.getByUserId(user._id).subscribe(({ badges }) => {
+        badges.forEach(badge => {
+          this.myInsigniaSection.stickers.push({
+            avatar: badge.logo,
+            title: `¡Felicidades ${ user.nickname }! Has obtenido la insignia ${ badge.title }`,
+            subtitle: badge.challenge
+          });
+        });
       });
     });
 

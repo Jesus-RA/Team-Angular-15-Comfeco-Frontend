0ca926bba50d2fcdd826647b0def601abe56f734
Ivan Zaldivar <ivanzaldivar16@gmail.com>
2021-03-18 02:16:22 -0600

    It can be filtered by programming language.

--- src/app/components/groups-container/groups-container.component.ts
+++ src/app/components/groups-container/groups-container.component.ts
@@ -6,6 +6,7 @@ import { Group } from 'src/app/services/group/interfaces/group.interfaces';
 
 // Imports services.
 import { GroupService } from 'src/app/services/group/group.service';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-groups-container',
@@ -13,13 +14,26 @@ import { GroupService } from 'src/app/services/group/group.service';
   styleUrls: ['./groups-container.component.css']
 })
 export class GroupsContainerComponent implements OnInit {
+  languages: Set<string>;
   groups: Group[] = [];
 
   constructor(private groupService: GroupService) {}
 
   ngOnInit(): void {
     this.groupService.list().subscribe(({ groups }) => {
+      const items = groups.map(group => group.tag.text);
+      this.languages = new Set(items);
       this.groups = groups;
+    });
+  }
+
+  filter(event: MatSelectChange): void {
+    const elements = document.querySelectorAll("#group-card") as NodeListOf<HTMLElement>;
+    elements.forEach(element => {
+      const word: string = element.className.toLocaleLowerCase();
+      let keyword = event.value as string;
+      const status: boolean = word.indexOf(keyword.toLocaleLowerCase()) !== -1 ? true : false;
+      this.conditionRendering(element.parentElement, status);
     });
   }
 
@@ -30,12 +44,16 @@ export class GroupsContainerComponent implements OnInit {
     elements.forEach(element => {
       const keyword: string = input.value.toLocaleLowerCase();
       const word: string = element.textContent.toLocaleLowerCase();
-      
-      if (word.indexOf(keyword) !== -1) {
-        element.parentElement.style.display = "block";
-      } else {
-        element.parentElement.style.display = "none";
-      }
+      const status: boolean = word.indexOf(keyword) !== -1 ? true : false;
+      this.conditionRendering(element.parentElement, status);
     });
+  }
+
+  private conditionRendering(element: HTMLElement, status: boolean): void {
+    if (status) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
   }
 }

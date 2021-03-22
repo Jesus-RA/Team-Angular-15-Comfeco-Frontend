61635db3f7b1141956dd0244d702610f8ae67fa9
Jesus-RA <jesus.ra98@hotmail.com>
2021-03-21 19:33:41 -0600

    Builded to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: "http://localhost:5050/api/v1",
+  GOOGLE_CLIENT_ID: "495092721571-pl56bohv6e5853s923d5k69d6heuqtbp.apps.googleusercontent.com",
+  FACEBOOK_CLIENT_ID: "1144377869337240"
 };
